66c30a8c6a885018ba4cac25ef56e8481b252569
jxblrkrstffrbrms <jaxblairekristofferramos@gmail.com>
2022-07-22 20:58:09 +0800

    added contents on the pages

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['tab4.page.scss']
 })
 export class Tab4Page {
+  age = '20 years old';
+  bday = 'August 3, 2001';
+  address = 'San Pascual, Batangas';
+  height = '175 cm';
+  weight = '45 kilograms';
 
   constructor() {}
 
